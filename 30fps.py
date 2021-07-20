817c4769b7f4719f7ac3e50b45c875e4ddf4a9a9
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2021-07-20 10:05:09 +0700

    update 20/7

--- 30fps.py
+++ 30fps.py
@@ -6,22 +6,18 @@ import sys
 import config
 import fpstimer
 
-timer = fpstimer.FPSTimer(60)
+timer = fpstimer.FPSTimer(120)
 
 class MainWindow(QMainWindow, QDialog):
     def __init__(self): #khi ép object vô class thì luôn chạy hàm constructor này
         super(MainWindow, self).__init__()
-        self.initUI() #khi chạy sẽ gọi hàm initUI
-        self.x=0
-
-    def initUI(self): #set các thông số của cửa sổ game
         self.setGeometry(0, 0, 1920, 1080) #kích thước fullHD
         self.setWindowTitle("Game cua Nghi")
-        self.startup() #gọi hàm startup
         self.showMaximized() #để phóng to fullscreen cửa sổ game
+        self.startup()
+        self.runGame()
 
     def startup(self):
-        global timer
         self.background=QLabel(self) #xem QLabel như là 1 cái khuôn nhỏ khác, ép object background vào
         self.background.setScaledContents(True)
         self.background.setPixmap(QPixmap(config.background)) #set hình cho object tên là background
@@ -32,10 +28,10 @@ class MainWindow(QMainWindow, QDialog):
         self.target.setScaledContents(True)
         self.target.setPixmap(QPixmap(config.target)) #set hình mèo
         self.target.setGeometry(0, 400, 180, 270)
-        self.target.show()
 
-        for i in range(1920):
-            self.target.move(i, 500) #di chuyển đến tọa độ x y trên màn hình
+    def runGame(self):
+        for i in range(1720):
+            self.target.move(i, 400) #di chuyển đến tọa độ x y trên màn hình
             self.target.show() #hiện hình mèo ở vị trí đã di chuyển đến
             timer.sleep()
             pass
